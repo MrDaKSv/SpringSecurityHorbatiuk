9d6c53d2f38adf44bb7a9f97de36e1fe8e4e17e3
MrDaKSv <bodagarbatuk@gmail.com>
2025-09-29 20:39:42 +0300

    Init commit

---
+++ src/main/java/edu/horb/springHorb/SpringHorbApplication.java
@@ -0,0 +1,13 @@
+package edu.horb.springHorb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHorbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHorbApplication.class, args);
+	}
+
+}
